41aaabd064c4a9d7362cfafea2d1945875564237
sgayathri82 <116699155+sgayathri82@users.noreply.github.com>
2022-10-27 08:21:12 +0800

    Add files via upload
    week1day1Assignment

---
+++ day1/Car.java
@@ -0,0 +1,34 @@
+package day1;
+
+public class Car {
+
+		public void driveCar() {
+
+			System.out.println("Drive the Car on Green Light ");
+		}
+		
+		public void applyBrake() {
+
+			System.out.println("Apply Brake on Yellow Light ");
+		}
+		
+		public void soundHorn() {
+
+			System.out.println("Sound Horn during Traffic ");
+		}
+
+		public void isPuncture() {
+
+			System.out.println("Change the Punctured Tyre ");
+		}
+		
+		public static void main(String[] args) {
+			Car obj = new Car();
+			obj.driveCar();
+			obj.applyBrake();
+			obj.soundHorn();
+			obj.isPuncture();
+			
+		}
+	
+}
