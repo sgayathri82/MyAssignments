c1230fbb762808e3d46e935c6091bce75b07ec6e
sgayathri82 <116699155+sgayathri82@users.noreply.github.com>
2022-11-01 09:38:13 +0800

    Add files via upload

---
+++ day1/FirstProgram.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	//main cntr + space
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//syso - cntr + space
+		
+		System.out.println("Welcome to TestLeaf");
+		
+		System.out.println("My First Program");
+
+	}
+
+}
---
+++ day1/SecondProgram.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class SecondProgram {
+	
+	public int getPhoneNum() {
+		return 123456;
+	
+	}
+	public String getPhoneColor() {
+		return "red";
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
+
+	
